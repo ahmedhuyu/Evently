5f5b7a64c17d19397dc3223f69a1333ff7e8157d
Ahmed Yunis <ahmed.huyu@gmail.com>
2024-09-22 14:06:29 +0300

    to deploy in vercel

---
+++ lib/database/models/order.model.ts
@@ -0,0 +1,42 @@
+import { model, models, Schema } from "mongoose";
+
+
+//defining the types of Order interface
+export interface IOrder extends Document{
+    createdAt: Date
+    stripeId: String
+    totalAmount: string
+    event: {
+        _id: string
+        firstName: string
+        lastName: string
+    }
+}
+
+//defining Properties
+const OrderSchema = new Schema({
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    stripeId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    totalAmount: {
+        type: String,
+    },
+    event: {
+        type: Schema.Types.ObjectId,
+        ref: 'Event',
+    },
+    buyer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+const Order = models.Order || model('Order', OrderSchema)
+
+export default Order;
